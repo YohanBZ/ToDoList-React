0dc47c23858b05932f76843bad5c68ffc57b73ed
YohanBZ <barbozayohan@gmail.com>
2023-10-14 21:56:42 -0300

    feat: :sparkles: Criação do componente Header

---
+++ src/components/Header/HeaderStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HeaderStyles = styled.header`
+  height: 100px;
+  width: 100%;
+  background-color: #14203f;
+  display: flex;
+  padding: 2rem;
+  justify-content: space-between;
+  align-items: center;
+`;
