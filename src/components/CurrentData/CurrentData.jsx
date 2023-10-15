3db47100b9a77a5e00eaaf6523d2e7c248d05157
YohanBZ <barbozayohan@gmail.com>
2023-10-14 22:00:54 -0300

    feat: :art: criação do componente currentData

---
+++ src/components/CurrentData/CurrentData.jsx
@@ -0,0 +1,18 @@
+import { Calendar } from '@phosphor-icons/react';
+import React from 'react';
+
+const CurrentDate = () => {
+  const currentDate = new Date();
+  const formattedDate = `${currentDate.getDate()}/${
+    currentDate.getMonth() + 1
+  }/${currentDate.getFullYear()}`;
+
+  return (
+    <div style={{ color: '#c0c5de', alignItems: 'center' }}>
+      <Calendar size={24} color="#c0c5de" />
+      {formattedDate}
+    </div>
+  );
+};
+
+export default CurrentDate;
