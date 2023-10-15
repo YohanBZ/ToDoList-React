dba536c6e031f4a32f3021d19956b44d9dd4f14b
YohanBZ <barbozayohan@gmail.com>
2023-10-14 21:57:59 -0300

    fix: :zap: Criação do componente depesquisa

---
+++ src/components/Seach/SearchStyled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const SearchStyled = styled.input`
+  width: 300px;
+  padding: 0.5rem 1rem;
+  border-radius: 10px;
+  border: none;
+  color: white;
+  background-color: #2d3954;
+`;
